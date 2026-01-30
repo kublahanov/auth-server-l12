356f7a9cf9deeb1192787e34735e6e1a8e2cd003
Roman Kondrashov <nr.kondrashov@gmail.com>
2026-01-30 23:35:49 +0300

    Исправил названия образов. Добавил пакет "tymon/jwt-auth". Удалил пакет "laravel/sail". Добавил в сборку PHP модуль pcntl.

--- .local/remove_images.sh
+++ .local/remove_images.sh
@@ -2,4 +2,4 @@
 
 # Скрипт для удаления Docker образов проекта
 
-docker rmi auth_api-php83:latest auth_api-caddy:latest auth_api-postgres:latest auth_api-mailpit:latest
+docker rmi auth_api_php83:latest auth_api_caddy:latest auth_api_postgres:latest auth_api_mailpit:latest
